ddee2a323b2eed5a26065f4386a796690625c243
Christian Lilley <cmlpub@lilfin.net>
2013-09-12 13:28:47 +0530

    Improvements: mostly in comments, and ensuring it's not broken when it opens. Also, network error handler.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 /* The basic control-flow we want works something like this:
 
                   document.ready?
@@ -22,9 +24,7 @@ Among other things, promises *are* a messaging system.
 // and letting all our promises hang out as globals. 
 
 
-
 /*  SOME UTILITY FUNCTIONS WE'LL USE IN OUR CODE, BUT DON'T HAVE TO WRITE  */
-
 function timeoutCallback() {
     // we'll use this as the handler for our non-Promise Timeouts
     alert('timeout reached');
@@ -33,6 +33,11 @@ function valueLogger(value) {
     // handler to show us the values we get back from async functions
     console.log(value);
 }
+function errorLogger(XHR) {
+    networkError = Q(XHR).inspect();
+    console.log("Network operation failed. Error follows:")
+    console.log(XHR.error());
+}
 function writeToDOM() {
     $('#insertedContent').append("<h2 class='text-info'>This wouldn't exist if docPromise hadn't resolved already...</h2>")
 }
@@ -40,7 +45,7 @@ function writeToDOMAgain() {
     $('#insertedContent').append("<h3 class='text-success'>And <em>this</em> wouldn't exist if all promises on the whole page hadn't resolved!</h3>")   
 }
 
-/*  THE FUNCTIONS THAT SUPPORT OUR DEMO  */
+/*  THE FUNCTIONS THAT SUPPORT OUR DEMO BUTTONS */
 function createDeferred() {
     sampleDeferred = Q.defer();
     samplePromise = sampleDeferred.promise;
@@ -65,14 +70,12 @@ function resolveDeferred() {
 function rejectDeferred() {
     sampleDeferred.reject("Rejections require a reason!")
 }
-
-
 function delayOld(ms) {
-    // create an old-fashioned style callback-based timout. Simple! 
-    // IMPLEMENT
-    
+    // old-fashioned style callback-based timout.
+    window.setTimeout(timeoutCallback, ms);
 };
 
+/*  STUFF FOR YOU TO IMPLEMENT */
 function delayNew(ms) {
     // the new, Promises-based version of a simple setTimeout.
     // Starts the timer, establishes callback handler for timer, then returns a promise right away. 
@@ -83,6 +86,11 @@ function delayNew(ms) {
     // Your code follows...
     // IMPLEMENT
     
+
+
+
+
+
 }
 
 function simpleWrapper(value) {
@@ -109,7 +117,7 @@ function stripOutMainContent(wholePage) {
 
 function writeArticleToDOM(articleText) {
     $('article').html(articleText);
-    // and, just as good form, return a promise to the end of the chain, so we see the output:
+    // and, just as good form, return a promise to the end of the chain, containing articleText, so we can see the output:
     // IMPLEMENT
     
 }
@@ -118,48 +126,73 @@ function doMoreStuff() {
     // we're going to wait for the document.ready promise to resolve. 
     // We're going to immediately create three new promises, and an aggregate promise based on them. 
 
-    // Go do a real Network Operation
+
+    // NETWORK OPERATION
     xhrPromise = $.ajax({ url: 'http://www.html5rocks.com/en/tutorials/file/xhr2/' });
     // Now, take that promise, stripOutMainContent and then writeArticleToDOM
+    // NOTE: stripOutMainContent still needs to be implemented!!!
     // (Wat? JQuery promises are weird... What do we do?)
-    compoundXHR = // IMPLEMENT
-    
+    // IMPLEMENT:
+    // compoundXHR = 
 
+    // Now, because this is a network operation, it can fail for all kinds of reasons. 
+    // Handle errors with the provided 'errorLogger' function. 
+    // IMPLEMENT:
+
+    // NETWORK OPERATION ENDS
+
+
+    // SIMPLE VALUE PROMISE
     // create a simple value-based promise using function we already created, and this text:
     var simpleValue = 'I bet this promise resolved first among the three...';
-    simplePromise = // IMPLEMENT
-    // Now, access this promise and just view the content:
+    // IMPLEMENT
+    // simplePromise = 
+    // Now, access this promise and just log the content:
     // IMPLEMENT
 
-    // Create a new promise based on a simple Timeout, using our promise-generating delayNew Function;
-    delayPromise = delayNew(2500);
-    // Alternately, we could actually use Q's convenience method for timeouts, which returns a promise:
-    // ex.: delayPromise = Q.delay(2500)
+    // SIMPLE VALUE PROMISE ENDS
 
-    // Now, access this promise and just view the content:
+
+    // TIMEOUT PROMISE
+    // Create a new promise based on a Timeout of 2500ms, using our promise-generating delayNew Function;
+    // IMPLEMENT
+    // delayPromise = 
+    /* ( Side-note: Alternately, we could actually use Q's convenience method for timeouts, which returns a promise:
+            ex.: delayPromise = Q.delay(2500) ) */
+
+    // Now, access this delayPromise and just log the content:
     // IMPLEMENT
 
+    // TIMEOUT PROMISE ENDS
+
+
+    // FINAL HANDLER FOR ALL THREE OF THOSE PROMISES ABOVE
     // The .all() method returns a new, aggregated, promise. 
     // It's resolved when an entire array of promises are each resolved.
-    // We want an aggregate promise for simplePromise, compoundXHR & delayPromise
+    // We want an aggregate promise composed of simplePromise, compoundXHR & delayPromise
+    // Then, log that promise
+    // Then, run writeToDOMAgain
     // IMPLEMENT
 }
 
 
-// Create a promise based on the document's 'ready' event
-
-docDeferred = // IMPLEMENT
-docPromise = // IMPLEMENT
+// Create a deferred and a promise (We'll use them to represent the state of the document's 'ready' event.)
+// IMPLEMENT
+// docDeferred =
+// IMPLEMENT 
+// docPromise = 
 $('document').ready(function(){
     console.log('Doc Ready. No other functions ready yet.') // disable me when more interesting things are happening
     var readyMessage = 'Document is ready now. Go Ahead and do more promise stuff!';
     // This is where you should resolve the docPromise, with the message above...
     // IMPLEMENT
+
 }) 
 // Create handlers for the docPromise that:
 // 1. Just shows its value 
-// 2. Uses the 'writeToDom' function to print it out
+// 2. Uses the 'writeToDOM' function to print it out
 // 3. continues our control-flow with doMoreStuff !
 
 // IMPLEMENT
+
 
